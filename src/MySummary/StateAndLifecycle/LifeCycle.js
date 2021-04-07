8b533316dd91ad1b1320b6488dee959e8978a4b8
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-04-07 14:32:59 +0200

    LifeCycle

---
+++ src/MySummary/StateAndLifecycle/LifeCycle.js
@@ -0,0 +1,32 @@
+// componentWillMount() 
+// immediately befor initial rendering
+
+//componentDidMount()
+// immediately after initial rendering
+
+//componentWillReceiveProps()
+// when component recive new props
+
+//shouldComponentUpdate()
+// befor rendering, after reciving new props or state
+// (return false to prevent rendering)
+
+//componentWillUpdate()
+// befor rendering, after reciving new props or state
+
+//componentDidUpdate()
+// after component's updates are flushed to DOM
+
+// componentWillUnmount()
+// immediately befor removing component from DOM
+
+// =================================
+
+// Order of Execution
+    // 1- construvtor
+    // 2- component Will mount
+    // 3- component Did mount
+    // *- component Will Receive Props => when changing something
+    // 4- should Component Update
+    // 5- component Will Update
+    // 6- component Did Update
