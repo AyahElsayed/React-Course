7800464e8a8296ddc023ad318429e95684115c9d
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-04-13 16:38:46 +0200

    improving and fixing

--- src/MySummary/Hooks/UseEffect/ImageChangeOnScroll.js
+++ src/MySummary/Hooks/UseEffect/ImageChangeOnScroll.js
@@ -1,23 +1,39 @@
-import React from 'react'
+import React, { useState ,useEffect} from 'react'
 import ImageToggleOnMouseOver from './ImageToggleOnScroll'
 
 function ImageChangeOnScroll() {
+    const [currentID, setCurrentID] = useState(0)
+    const ID = [10, 20, 30, 40,50,60]
+    useEffect(() => {
+        window.document.title = `ID: ${currentID}` 
+        
+    },[currentID])
     return (
         <div>
-            <ImageToggleOnMouseOver firstImg="./assets/work2.webp"
-                secImg="./assets/work4.webp"
-                alt="" />
+            {
+                ID.map((singleId) => {
+                    return <div key={singleId}
+                        onMouseOver={() => {
+                            setCurrentID(singleId)
+                            console.log(`onMouseOver:${singleId}`)
+                        }}>
+                        <ImageToggleOnMouseOver firstImg="./assets/work2.webp"
+                            secImg="./assets/work4.webp"
+                            alt="" />
             &nbsp; &nbsp; &nbsp;
             <ImageToggleOnMouseOver firstImg="./assets/work5.webp"
-                secImg="./assets/work6.webp"
-                alt="" />
-            <ImageToggleOnMouseOver firstImg="./assets/work2.webp"
-                secImg="./assets/work4.webp"
-                alt="" />
+                            secImg="./assets/work6.webp"
+                            alt="" />
+                        {/* <ImageToggleOnMouseOver firstImg="./assets/work2.webp"
+                            secImg="./assets/work4.webp"
+                            alt="" />
             &nbsp; &nbsp; &nbsp;
             <ImageToggleOnMouseOver firstImg="./assets/work5.webp"
-                secImg="./assets/work6.webp"
-                alt="" />
+                            secImg="./assets/work6.webp"
+                            alt="" /> */}
+                    </div>
+                })
+            }
         </div>
     )
 }
